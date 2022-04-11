3f0b38de0439f85f790f05e3ee478b541251ce28
admin <admin@YD2-12>
2022-04-11 14:22:55 +0900

    ifExample2

---
+++ Yedam/src/co/edu/IfExample2.java
@@ -0,0 +1,21 @@
+package co.edu;
+
+public class IfExample2 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		if (num1 > num2) {
+			System.out.println(num1 + "이 큽니다.");
+		} else {
+			System.out.println(num1+" 는 " + num2 + "보다 작다.");
+		}
+		
+		
+		
+
+	}
+
+}
